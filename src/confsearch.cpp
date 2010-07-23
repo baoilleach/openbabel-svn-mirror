2e6dca6a9243c944d59172a2ea6a84c5ded2d1d0
baoilleach <baoilleach@gmail.com>
2010-07-23 15:14:40 +0100

    Initial commit of DiversePose code

--- src/confsearch.cpp
+++ src/confsearch.cpp
@@ -22,12 +22,80 @@ GNU General Public License for more details.
 #include <openbabel/forcefield.h>
 #include <openbabel/rotamer.h>
 #include <openbabel/rotor.h>
+#include <openbabel/align.h>
+#include <openbabel/tree/tree.hh>
 
 #include <float.h> // For DBL_MAX
 #include <algorithm> // For min
 
 namespace OpenBabel
 {
+  
+  class DiversePoses {
+  public:
+    DiversePoses(double RMSD);
+    bool AddPose(const OBMol &mol);
+    typedef tree<OBMol> Tree;
+    typedef tree<OBMol>::iterator Tree_it;
+    typedef tree<OBMol>::sibling_iterator Tree_sit;
+    
+  private:
+    Tree poses;
+    std::vector<double> levels;
+    OBAlign align;
+    double cutoff;
+  };
+
+  DiversePoses::DiversePoses(double RMSD) {
+    cutoff = RMSD;
+
+    static const double arr[] = {3.0, 2.0, 1.5, 1.0, 0.5, 0.25};
+    std::vector<double> vec (arr, arr + sizeof(arr) / sizeof(arr[0]) );
+    vec.erase(std::remove_if(vec.begin(), vec.end(), std::bind1st(std::less<double>(), (cutoff + 0.1) )));
+    vec.push_back(cutoff);
+
+    levels = vec;
+  }
+
+  bool DiversePoses::AddPose(const OBMol &mol) {
+    OBMol cmol = mol; // Store a copy of the molecule in the tree
+
+    Tree_it node = poses.begin();
+    int level = 0;
+
+    while(true) {
+
+      // Find whether the molecule is similar to any of the siblings of this node.
+      // - min_node will hold the result of this search
+      align.SetRefMol(mol);
+      Tree_it min_node = NULL;
+      Tree_sit sib;
+      for (sib = poses.begin(node); sib != poses.end(node); ++sib) { // Iterate over siblings of node
+        align.SetTargetMol(*sib);
+        align.Align();
+        double rmsd = align.GetRMSD();
+        if (rmsd < levels.at(level)) {
+          if (rmsd < cutoff)
+            return false;
+          min_node = sib;
+          break; // Exit as soon as one is found
+        }
+      } // end of for loop
+
+      if (min_node == NULL) { // No similar molecule found, so append it the siblings
+        // At this point, sib will be equal to poses.end(node)
+        poses.insert(sib, cmol);
+
+
+
+      }
+      
+    } // end of while loop
+
+    return true;
+
+  }
+
   int OBForceField::FastRotorSearch(bool permute)
   {
     if (_mol.NumRotors() == 0)
