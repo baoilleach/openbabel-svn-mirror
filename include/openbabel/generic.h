baoilleach/openbabel-svn-mirror

commit 76bb8ae1426319dd1cd8652f1a79254c5da581ed

Fix const-ness of OpenBabel::OBUnitCell.
git-svn-id: http://openbabel.svn.sourceforge.net/svnroot/openbabel/openbabel/trunk@4401 71e0b675-c112-0410-beda-96664073b5c2

include/openbabel/generic.h
@@ -375,34 +375,34 @@ namespace OpenBabel
     void FillUnitCell(OBMol *);
 
     //! \return vector a
-    double GetA();
+    double GetA() const;
     //! \return vector b
-    double GetB();
+    double GetB() const;
     //! \return vector c
-    double GetC();
+    double GetC() const;
     //! \return angle alpha
-    double GetAlpha();
+    double GetAlpha() const;
     //! \return angle beta
-    double GetBeta();
+    double GetBeta() const;
     //! \return angle gamma
-    double GetGamma();
+    double GetGamma() const;
     //! \return any offset in the origin of the periodic boundaries
-    vector3 GetOffset();
+    vector3 GetOffset() const;
 
     //! \return the text representation of the space group for this unit cell
-    const SpaceGroup* GetSpaceGroup() { return(_spaceGroup); }
+    const SpaceGroup* GetSpaceGroup() const { return(_spaceGroup); }
 
     //! \return the text representation of the space group for this unit cell
-    const std::string GetSpaceGroupName() { return(_spaceGroupName); }
+    const std::string GetSpaceGroupName() const { return(_spaceGroupName); }
 
     //! \return lattice type (based on the @p spacegroup)
-    LatticeType GetLatticeType( int spacegroup );
+    LatticeType GetLatticeType( int spacegroup ) const;
 
     //! \return lattice type (based on angles and distances)
-    LatticeType GetLatticeType();
+    LatticeType GetLatticeType() const;
 
     //! \return v1, v2, v3 cell vectors
-    std::vector<vector3> GetCellVectors();
+    std::vector<vector3> GetCellVectors() const;
     //! Access to the cell matrix as row vectors, useful for writing input files.
     //! Equivalent to the transpose of GetOrientationMatrix() * GetOrthoMatrix()
     //! \return The cell matrix with row vectors.
@@ -411,14 +411,14 @@ namespace OpenBabel
     //! \see OBUnitCell::GetOrientationMatrix
     //! \see OBUnitCell::FractionalToCartesian
     //! \see OBUnitCell::CartesianToFractional
-    matrix3x3	GetCellMatrix();
+    matrix3x3	GetCellMatrix() const;
     //! \return The orthogonalization matrix, used for converting from fractional to Cartesian coords.
     //! \see OBUnitCell::GetCellMatrix
     //! \see OBUnitCell::GetFractionalMatrix
     //! \see OBUnitCell::GetOrientationMatrix
     //! \see OBUnitCell::FractionalToCartesian
     //! \see OBUnitCell::CartesianToFractional
-    matrix3x3 GetOrthoMatrix();
+    matrix3x3 GetOrthoMatrix() const;
     //! Used to convert fractional and cartesian coordinates if the
     //! cell is not oriented in standard form (a parallel to x axis,
     //! b in xy plane)
@@ -428,14 +428,14 @@ namespace OpenBabel
     //! \see OBUnitCell::GetFractionalMatrix
     //! \see OBUnitCell::FractionalToCartesian
     //! \see OBUnitCell::CartesianToFractional
-    matrix3x3 GetOrientationMatrix();
+    matrix3x3 GetOrientationMatrix() const;
     //! \return The fractionalization matrix, used for converting from Cartesian to fractional coords.
     //! \see OBUnitCell::GetOrthoMatrix
     //! \see OBUnitCell::GetCellMatrix
     //! \see OBUnitCell::GetOrientationMatrix
     //! \see OBUnitCell::FractionalToCartesian
     //! \see OBUnitCell::CartesianToFractional
-    matrix3x3 GetFractionalMatrix();
+    matrix3x3 GetFractionalMatrix() const;
 
     //! Convenience function to convert fractional coordinates to
     //! cartesian coordinates. Returns
@@ -443,28 +443,28 @@ namespace OpenBabel
     //! GetOrientationMatrix() * GetOrthoMatrix() * frac + GetOffset()
     //! \param frac Vector of fractional coordinates
     //! \return Cartesian coordinates
-    vector3 FractionalToCartesian(vector3 frac);
+    vector3 FractionalToCartesian(vector3 frac) const;
     //! Convenience function to convert cartesian coordinates to
     //! fractional coordinates. Returns
     //!
     //! GetFractionalMatrix() * GetOrientationMatrix().inverse() * (cart - GetOffset())
     //! \param cart Vector of cartesian coordinates
     //! \return Fractional coordinates
-    vector3 CartesianToFractional(vector3 cart);
+    vector3 CartesianToFractional(vector3 cart) const;
 
     //! Wraps cartesian coordinate to fall within the unit cell.
     //! \param cart Vector of cartesian coordinates
     //! \return Cartesian coordinates within cell boundaries.
-    vector3 WrapCartesianCoordinate(vector3 cart);
+    vector3 WrapCartesianCoordinate(vector3 cart) const;
     //! Wraps fractional coordinate to fall within the unit cell.
     //! \param frac Vector of fractional coordinates
     //! \return Fractional coordinates within cell boundaries (between 0 and 1).
-    vector3 WrapFractionalCoordinate(vector3 frac);
+    static vector3 WrapFractionalCoordinate(vector3 frac);
 
     //! \return The numeric value of the given spacegroup
-    int GetSpaceGroupNumber( std::string name = "" );
+    int GetSpaceGroupNumber( std::string name = "" ) const;
     //! \return The cell volume (in Angstroms^3)
-    double GetCellVolume();
+    double GetCellVolume() const;
   };
 
   //! \class OBConformerData generic.h <openbabel/generic.h>

src/generic.cpp
@@ -319,7 +319,7 @@ namespace OpenBabel
   }
 
   //! Implements <a href="http://qsar.sourceforge.net/dicts/blue-obelisk/index.xhtml#convertNotionalIntoCartesianCoordinates">blue-obelisk:convertNotionalIntoCartesianCoordinates</a>
-  vector<vector3> OBUnitCell::GetCellVectors()
+  vector<vector3> OBUnitCell::GetCellVectors() const
   {
     vector<vector3> v;
     v.reserve(3);
@@ -333,37 +333,37 @@ namespace OpenBabel
     return v;
   }
 
-  matrix3x3 OBUnitCell::GetCellMatrix()
+  matrix3x3 OBUnitCell::GetCellMatrix() const
   {
     return (_mOrient * _mOrtho).transpose();
   }
 
-  matrix3x3 OBUnitCell::GetOrthoMatrix()
+  matrix3x3 OBUnitCell::GetOrthoMatrix() const
   {
     return _mOrtho;
   }
 
-  matrix3x3 OBUnitCell::GetOrientationMatrix()
+  matrix3x3 OBUnitCell::GetOrientationMatrix() const
   {
     return _mOrient;
   }
 
-  matrix3x3 OBUnitCell::GetFractionalMatrix()
+  matrix3x3 OBUnitCell::GetFractionalMatrix() const
   {
     return _mOrtho.inverse();
   }
 
-  vector3 OBUnitCell::FractionalToCartesian(vector3 frac)
+  vector3 OBUnitCell::FractionalToCartesian(vector3 frac) const
   {
     return _mOrient * _mOrtho * frac + _offset;
   }
 
-  vector3 OBUnitCell::CartesianToFractional(vector3 cart)
+  vector3 OBUnitCell::CartesianToFractional(vector3 cart) const
   {
     return _mOrtho.inverse() * _mOrient.inverse() * (cart - _offset);
   }
 
-  vector3 OBUnitCell::WrapCartesianCoordinate(vector3 cart)
+  vector3 OBUnitCell::WrapCartesianCoordinate(vector3 cart) const
   {
     vector3 v = CartesianToFractional(cart);
     v = WrapFractionalCoordinate(v);
@@ -381,7 +381,7 @@ namespace OpenBabel
     return vector3(x, y, z);
   }
 
-  OBUnitCell::LatticeType OBUnitCell::GetLatticeType( int spacegroup )
+  OBUnitCell::LatticeType OBUnitCell::GetLatticeType( int spacegroup ) const
   {
     //	1-2 	Triclinic
     //	3-15 	Monoclinic
@@ -430,7 +430,7 @@ namespace OpenBabel
       return OBUnitCell::Undefined;
   }
 
-  OBUnitCell::LatticeType OBUnitCell::GetLatticeType()
+  OBUnitCell::LatticeType OBUnitCell::GetLatticeType() const
   {
     if (_lattice != Undefined)
       return _lattice;
@@ -449,36 +449,40 @@ namespace OpenBabel
     if (IsApprox(beta,  90.0, 1.0e-3)) rightAngles++;
     if (IsApprox(gamma, 90.0, 1.0e-3)) rightAngles++;
 
+    // recast cache member "_lattice" as mutable
+    OBUnitCell::LatticeType *lattice =
+      const_cast<OBUnitCell::LatticeType*>(&_lattice);
+
     switch (rightAngles)
       {
       case 3:
         if (IsApprox(a, b, 1.0e-4) && IsApprox(b, c, 1.0e-4))
-          _lattice = Cubic;
+          *lattice = Cubic;
         else if (IsApprox(a, b, 1.0e-4) || IsApprox(b, c, 1.0e-4))
-          _lattice = Tetragonal;
+          *lattice = Tetragonal;
         else
-          _lattice = Orthorhombic;
+          *lattice = Orthorhombic;
         break;
       case 2:
         if ( (IsApprox(alpha, 120.0, 1.0e-3)
               || IsApprox(beta, 120.0, 1.0e-3)
               || IsApprox(gamma, 120.0f, 1.0e-3))
              && (IsApprox(a, b, 1.0e-4) || IsApprox(b, c, 1.0e-4)) )
-          _lattice = Hexagonal;
+          *lattice = Hexagonal;
         else
-          _lattice = Monoclinic;
+          *lattice = Monoclinic;
         break;
       default:
         if (IsApprox(a, b, 1.0e-4) && IsApprox(b, c, 1.0e-4))
-          _lattice = Rhombohedral;
+          *lattice = Rhombohedral;
         else
-          _lattice = Triclinic;
+          *lattice = Triclinic;
       }
 
-    return _lattice;
+    return *lattice;
   }
 
-  int OBUnitCell::GetSpaceGroupNumber( std::string name)
+  int OBUnitCell::GetSpaceGroupNumber( std::string name) const
   {
     static const char * const spacegroups[] = {
       "P1", "P-1", "P2", "P2(1)", "C2", "Pm", "Pc", "Cm", "Cc", "P2/m",
@@ -603,42 +607,42 @@ namespace OpenBabel
     SetSpaceGroup(1); // We've now applied the symmetry, so we should act like a P1 unit cell
   }
 
-  double OBUnitCell::GetA()
+  double OBUnitCell::GetA() const
   {
     return _mOrtho.GetColumn(0).length();
   }
 
-  double OBUnitCell::GetB()
+  double OBUnitCell::GetB() const
   {
     return _mOrtho.GetColumn(1).length();
   }
 
-  double OBUnitCell::GetC()
+  double OBUnitCell::GetC() const
   {
     return _mOrtho.GetColumn(2).length();
   }
 
-  double OBUnitCell::GetAlpha()
+  double OBUnitCell::GetAlpha() const
   {
     return vectorAngle(_mOrtho.GetColumn(1), _mOrtho.GetColumn(2));
   }
 
-  double OBUnitCell::GetBeta()
+  double OBUnitCell::GetBeta() const
   {
     return vectorAngle(_mOrtho.GetColumn(0), _mOrtho.GetColumn(2));
   }
 
-  double OBUnitCell::GetGamma()
+  double OBUnitCell::GetGamma() const
   {
     return vectorAngle(_mOrtho.GetColumn(0), _mOrtho.GetColumn(1));
   }
 
-  vector3 OBUnitCell::GetOffset()
+  vector3 OBUnitCell::GetOffset() const
   {
     return _offset;
   }
 
-  double OBUnitCell::GetCellVolume()
+  double OBUnitCell::GetCellVolume() const
   {
     return fabs(GetCellMatrix().determinant());
   }

commit 2fae89427ab6e38c8194a88018a355879e9fec25

Fix bug PR#3411738 Restore ABI from break in r4401.
Adding the const keyword broke binary compatibility in r4401. This patch
adds the non-const overloads back. Thanks to Andrey Ponomarenko for
pointing this out.

git-svn-id: http://openbabel.svn.sourceforge.net/svnroot/openbabel/openbabel/trunk@4599 71e0b675-c112-0410-beda-96664073b5c2

include/openbabel/generic.h
@@ -374,34 +374,48 @@ namespace OpenBabel
     //! of the molecule, based on the known space group
     void FillUnitCell(OBMol *);
 
+    //! @todo Remove nonconst overloads in OBUnitCell on next version bump
+
     //! \return vector a
+    double GetA();
     double GetA() const;
     //! \return vector b
+    double GetB();
     double GetB() const;
     //! \return vector c
+    double GetC();
     double GetC() const;
     //! \return angle alpha
+    double GetAlpha();
     double GetAlpha() const;
     //! \return angle beta
+    double GetBeta();
     double GetBeta() const;
     //! \return angle gamma
+    double GetGamma();
     double GetGamma() const;
     //! \return any offset in the origin of the periodic boundaries
+    vector3 GetOffset();
     vector3 GetOffset() const;
 
     //! \return the text representation of the space group for this unit cell
+    const SpaceGroup* GetSpaceGroup() { return(_spaceGroup); }
     const SpaceGroup* GetSpaceGroup() const { return(_spaceGroup); }
 
     //! \return the text representation of the space group for this unit cell
+    const std::string GetSpaceGroupName() { return(_spaceGroupName); }
     const std::string GetSpaceGroupName() const { return(_spaceGroupName); }
 
     //! \return lattice type (based on the @p spacegroup)
+    LatticeType GetLatticeType( int spacegroup );
     LatticeType GetLatticeType( int spacegroup ) const;
 
     //! \return lattice type (based on angles and distances)
+    LatticeType GetLatticeType();
     LatticeType GetLatticeType() const;
 
     //! \return v1, v2, v3 cell vectors
+    std::vector<vector3> GetCellVectors();
     std::vector<vector3> GetCellVectors() const;
     //! Access to the cell matrix as row vectors, useful for writing input files.
     //! Equivalent to the transpose of GetOrientationMatrix() * GetOrthoMatrix()
@@ -411,6 +425,7 @@ namespace OpenBabel
     //! \see OBUnitCell::GetOrientationMatrix
     //! \see OBUnitCell::FractionalToCartesian
     //! \see OBUnitCell::CartesianToFractional
+    matrix3x3	GetCellMatrix();
     matrix3x3	GetCellMatrix() const;
     //! \return The orthogonalization matrix, used for converting from fractional to Cartesian coords.
     //! \see OBUnitCell::GetCellMatrix
@@ -418,6 +433,7 @@ namespace OpenBabel
     //! \see OBUnitCell::GetOrientationMatrix
     //! \see OBUnitCell::FractionalToCartesian
     //! \see OBUnitCell::CartesianToFractional
+    matrix3x3 GetOrthoMatrix();
     matrix3x3 GetOrthoMatrix() const;
     //! Used to convert fractional and cartesian coordinates if the
     //! cell is not oriented in standard form (a parallel to x axis,
@@ -428,6 +444,7 @@ namespace OpenBabel
     //! \see OBUnitCell::GetFractionalMatrix
     //! \see OBUnitCell::FractionalToCartesian
     //! \see OBUnitCell::CartesianToFractional
+    matrix3x3 GetOrientationMatrix();
     matrix3x3 GetOrientationMatrix() const;
     //! \return The fractionalization matrix, used for converting from Cartesian to fractional coords.
     //! \see OBUnitCell::GetOrthoMatrix
@@ -435,6 +452,7 @@ namespace OpenBabel
     //! \see OBUnitCell::GetOrientationMatrix
     //! \see OBUnitCell::FractionalToCartesian
     //! \see OBUnitCell::CartesianToFractional
+    matrix3x3 GetFractionalMatrix();
     matrix3x3 GetFractionalMatrix() const;
 
     //! Convenience function to convert fractional coordinates to
@@ -443,6 +461,7 @@ namespace OpenBabel
     //! GetOrientationMatrix() * GetOrthoMatrix() * frac + GetOffset()
     //! \param frac Vector of fractional coordinates
     //! \return Cartesian coordinates
+    vector3 FractionalToCartesian(vector3 frac);
     vector3 FractionalToCartesian(vector3 frac) const;
     //! Convenience function to convert cartesian coordinates to
     //! fractional coordinates. Returns
@@ -450,11 +469,13 @@ namespace OpenBabel
     //! GetFractionalMatrix() * GetOrientationMatrix().inverse() * (cart - GetOffset())
     //! \param cart Vector of cartesian coordinates
     //! \return Fractional coordinates
+    vector3 CartesianToFractional(vector3 cart);
     vector3 CartesianToFractional(vector3 cart) const;
 
     //! Wraps cartesian coordinate to fall within the unit cell.
     //! \param cart Vector of cartesian coordinates
     //! \return Cartesian coordinates within cell boundaries.
+    vector3 WrapCartesianCoordinate(vector3 cart);
     vector3 WrapCartesianCoordinate(vector3 cart) const;
     //! Wraps fractional coordinate to fall within the unit cell.
     //! \param frac Vector of fractional coordinates
@@ -462,8 +483,10 @@ namespace OpenBabel
     static vector3 WrapFractionalCoordinate(vector3 frac);
 
     //! \return The numeric value of the given spacegroup
+    int GetSpaceGroupNumber( std::string name = "" );
     int GetSpaceGroupNumber( std::string name = "" ) const;
     //! \return The cell volume (in Angstroms^3)
+    double GetCellVolume();
     double GetCellVolume() const;
   };
 

src/generic.cpp
@@ -607,6 +607,36 @@ namespace OpenBabel
     SetSpaceGroup(1); // We've now applied the symmetry, so we should act like a P1 unit cell
   }
 
+  /// @todo Remove nonconst overloads in OBUnitCell on next version bump.
+#define OBUNITCELL_CALL_CONST_OVERLOAD(_type, _name) \
+  _type OBUnitCell::_name() \
+  { \
+    return const_cast<const OBUnitCell*>(this)->_name(); \
+  }
+#define OBUNITCELL_CALL_CONST_OVERLOAD_ARG(_type, _name, _argsig) \
+  _type OBUnitCell::_name( _argsig arg1 ) \
+  { \
+    return const_cast<const OBUnitCell*>(this)->_name(arg1); \
+  }
+  OBUNITCELL_CALL_CONST_OVERLOAD(double, GetA);
+  OBUNITCELL_CALL_CONST_OVERLOAD(double, GetB);
+  OBUNITCELL_CALL_CONST_OVERLOAD(double, GetC);
+  OBUNITCELL_CALL_CONST_OVERLOAD(double, GetAlpha);
+  OBUNITCELL_CALL_CONST_OVERLOAD(double, GetBeta);
+  OBUNITCELL_CALL_CONST_OVERLOAD(double, GetGamma);
+  OBUNITCELL_CALL_CONST_OVERLOAD_ARG(OBUnitCell::LatticeType,
+                                     GetLatticeType, int);
+  OBUNITCELL_CALL_CONST_OVERLOAD(OBUnitCell::LatticeType, GetLatticeType);
+  OBUNITCELL_CALL_CONST_OVERLOAD(std::vector<vector3>, GetCellVectors);
+  OBUNITCELL_CALL_CONST_OVERLOAD(matrix3x3, GetCellMatrix );
+  OBUNITCELL_CALL_CONST_OVERLOAD(matrix3x3, GetOrthoMatrix );
+  OBUNITCELL_CALL_CONST_OVERLOAD(matrix3x3, GetOrientationMatrix );
+  OBUNITCELL_CALL_CONST_OVERLOAD(matrix3x3, GetFractionalMatrix );
+  OBUNITCELL_CALL_CONST_OVERLOAD_ARG(vector3, FractionalToCartesian, vector3);
+  OBUNITCELL_CALL_CONST_OVERLOAD_ARG(vector3, CartesianToFractional, vector3);
+  OBUNITCELL_CALL_CONST_OVERLOAD_ARG(vector3, WrapCartesianCoordinate, vector3);
+  OBUNITCELL_CALL_CONST_OVERLOAD(double, GetCellVolume);
+
   double OBUnitCell::GetA() const
   {
     return _mOrtho.GetColumn(0).length();